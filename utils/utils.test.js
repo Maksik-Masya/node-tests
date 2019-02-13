aa773781ee6a02bcd41784c614d299f2f9193fb1
Maxym Borodenko <m.borodenko@aimprosoft.com>
2019-02-13 23:32:34 +0200

    Using an Assertion library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,39 @@
+const expect = require('expect');
 const utils = require('./util');
 
 it('should add two numbers', () => {
     const res = utils.add(33, 11);
 
-    if (res !== 44) {
-        throw new Error(`Expected 44, but was ${res}`);
-    }
+    expect(res).toBe(44).toBeA('number');
 });
 
 it('should count a square', () => {
     const res = utils.square(8);
 
-    if (res !== 64) {
-        throw new Error(`Expected 64, but was ${res}`);
-    }
+    expect(res).toBe(64).toBeA('number');
 });
+
+it('should set firstName and lastName to the user object', () => {
+    const user = { age: 27, location: 'Ukraine' };
+    const res = utils.setName(user, 'Max B');
+
+    expect(res).toInclude({
+       firstName: 'Max',
+       lastName: 'B'
+    }).toBeA('object');
+});
+
+// it('expect some value should be', () => {
+//    // expect(12).toNotBe(11);
+//    // expect({ name: "Max" }).toNotEqual({ name: "max" });
+//    // expect([1, 2, 3]).toExclude(4);
+//
+//     expect({
+//         name: 'Max',
+//         age: 27,
+//         location: 'Ukraine'
+//     }).toInclude({
+//         name: 'Max'
+//     });
+//
+// });
