4dda531f2539a0444de8e5306aec8ddff240fa89
Maxym Borodenko <m.borodenko@aimprosoft.com>
2019-02-14 13:16:36 +0200

    Testing Express applications: Part 2

--- server/server.js
+++ server/server.js
@@ -3,12 +3,23 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world!</h1>')
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    })
 });
 
-
-
+app.get('/users', (req, res) => {
+    const users = [
+        { name: 'Name1', age: 20 },
+        { name: 'Name2', age: 21 },
+        { name: 'Name3', age: 22 }
+    ];
+    res.status(200).send(users)
+});
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
+
+module.exports.app = app;
