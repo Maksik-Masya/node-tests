79dd0b851165aa687bd8a2661271af19f0a132fc
Maxym Borodenko <m.borodenko@aimprosoft.com>
2019-02-14 12:29:07 +0200

    Test asynchronous code

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -7,10 +7,24 @@ it('should add two numbers', () => {
     expect(res).toBe(44).toBeA('number');
 });
 
-it('should count a square', () => {
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(3, 4, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    })
+});
+
+it('should square a number', () => {
     const res = utils.square(8);
 
     expect(res).toBe(64).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+    utils.asyncSquare(3, (square) => {
+        expect(square).toBe(9).toBeA('number');
+        done();
+    })
 });
 
 it('should set firstName and lastName to the user object', () => {
